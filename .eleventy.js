997f73b0783cce5572e0866a45ab087a29e00abf
unisys12 <unisys12@gmail.com>
2024-03-20 14:26:31 -0500

    add api calls to dynamically display dogs

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const Fetch = require("@11ty/eleventy-fetch");
+
 module.exports = (config) => {
   config.setServerOptions({
     // Whether DOM diffing updates are applied where possible instead of page reloads
@@ -13,6 +15,27 @@ module.exports = (config) => {
   config.addPassthroughCopy("src/assets/imgs/*.png");
   config.addPassthroughCopy("src/favicon.ico");
 
+  config.addAsyncShortcode("currentDog", async (id) => {
+    const res = await selectedDog(id);
+    let pictures = new Array;
+    const selectedID = res.data.map((x) => x.id);
+    
+    res.included.map((items) => {
+      if (selectedID == id && items.type == 'pictures') {
+        pictures.push(items)
+      }
+    });
+      
+    return pictures.map((x) => `
+      <div class="group block overflow-hidden">
+        <img
+          src="${x.attributes.original.url}"
+          alt="foster image"
+          class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-auto"
+        />
+      </div>`)
+  });
+
   return {
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
@@ -20,5 +43,25 @@ module.exports = (config) => {
       input: "src",
       ouput: "_site",
     },
+  };
+
+  async function selectedDog(id) {
+    let url = `https://api.rescuegroups.org/v5/public/orgs/11256/animals/${id}`;
+
+    try {
+        return await Fetch(url, {
+            duration: "15m",
+            type: "json",
+            fetchOptions: {
+                method: "GET",
+                headers: {
+                    'Authorization': "DXYRIHS8",
+                    'Content-Type': "application/vnd.api+json"
+                }
+            }
+        })
+    } catch (error) {
+        console.error(`There was an error fetching your dogs from RescueGroups.org: ${error}`);
+    }
   };
 };
--- src/adoption.md
+++
@@ -1,6 +0,0 @@
----
-layout: 'base_layout.njk'
-page_description: 'How to become a Guardian Home for FTPF'
----
-
-{% include "adoption.html" %}
