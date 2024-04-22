3b6a7f688aa14568b76b6335cfbf17e5097b56c9
unisys12 <unisys12@gmail.com>
2024-04-21 20:52:13 -0500

    working example

--- workers/src/index.js
+++ workers/src/index.js
@@ -1,15 +1,29 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
+ * Let's try to make ../src/_data/dogs.js a worker
  *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * - process.env.ORG_ID is the Organization ID from Rescue Groups Dot Org
+ * - process.env.RESCUE_GROUPS_API_KEY is the API Key used for accessing their API
  */
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		let url = `https://api.rescuegroups.org/v5/public/orgs/${env.ORG_ID}/animals/search/available/`;
+		let body = {
+			"data": {
+				"filterRadius": {
+				"miles": 25,
+				"postalcode": 38834
+				}
+			}
+		};
+
+		return await fetch(url, {
+			method: "POST",
+			headers: {
+				'Authorization': `${env.RESCUE_GROUPS_API_KEY}`,
+				'Content-Type': "application/vnd.api+json"
+			},
+			body: JSON.stringify(body)
+		});
 	},
 };
