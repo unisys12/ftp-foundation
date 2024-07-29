488f1bcbdd4afcd8022371b5a78ccb0a8acc7371
unisys12 <unisys12@gmail.com>
2024-07-29 18:11:05 -0500

    add more sponsorships

--- src/_data/sponsor_ids.js
+++ src/_data/sponsor_ids.js
@@ -4,4 +4,19 @@ module.exports = [
     goal: "jbR8Xj",
     button: "gJKN4p",
   },
+  {
+    name: "Luna",
+    goal: "jN84Pg",
+    button: "gk2y0L",
+  },
+  {
+    name: "Lilly",
+    goal: "pA8xDg",
+    button: "L0x5GL",
+  },
+  {
+    name: "Max",
+    goal: "jN8KPg",
+    button: "gM8Y0L",
+  },
 ];
